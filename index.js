cd3e5e23f3a7ea13ba32e8a43b73a9d55cb4f4bf
freezegr <49855695+freezegr@users.noreply.github.com>
2021-04-09 12:55:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,130 +4,130 @@ exports.Server = class {
   constructor(ip) {
     if (!ip) throw Error('[FiveM]: you miss api!');
     this.req = request.defaults({
-      baseUrl: `http://${ip}/`
-    }),
-	this.getServerStatus = this.getServerStatus,
-	this.getOnlinePlayers = this.getOnlinePlayers,
-	this.getOnlinePlayersCount = this.getOnlinePlayersCount,
-	this.getMaxSlotPlayer = this.getMaxSlotPlayer,
-	this.getVersion = this.getVersion,
-	this.getTags = this.getTags,
-	this.getresources = this.getresources,
-	this.getLocale = this.getLocale,
-	this.getLicense = this.getLicense
+        baseUrl: `http://${ip}/`
+      }),
+      this.getServerStatus = this.getServerStatus,
+      this.getOnlinePlayers = this.getOnlinePlayers,
+      this.getOnlinePlayersCount = this.getOnlinePlayersCount,
+      this.getMaxSlotPlayer = this.getMaxSlotPlayer,
+      this.getVersion = this.getVersion,
+      this.getTags = this.getTags,
+      this.getresources = this.getresources,
+      this.getLocale = this.getLocale,
+      this.getLicense = this.getLicense
   };
-  
-  
-  getServerStatus(){
+
+
+  getServerStatus() {
     return new Promise((resolve, reject) => {
       this.req.get('info.json', (err, res, body) => {
-		if(err) return resolve('offline')
+        if (err) return resolve('offline')
         resolve('online')
-	  });
-	});
+      });
+    });
   };
-  
-  getOnlinePlayers(){
+
+  getOnlinePlayers() {
     return new Promise((resolve, reject) => {
-      this.req.get('players.json',{
+      this.req.get('players.json', {
         headers: {
           'Accept': 'application/json',
           'Accept-Charset': 'utf-8'
         }
-	  }, (err, res, body) => {
+      }, (err, res, body) => {
         resolve(JSON.parse(body))
-	  });
-	});
+      });
+    });
   };
-  
-  getOnlinePlayersCount(){
+
+  getOnlinePlayersCount() {
     return new Promise((resolve, reject) => {
-      this.req.get('players.json',{
+      this.req.get('players.json', {
         headers: {
           'Accept': 'application/json',
           'Accept-Charset': 'utf-8'
         }
-	  }, (err, res, body) => {
+      }, (err, res, body) => {
         resolve(JSON.parse(body).length)
-	  });
-	});
+      });
+    });
   };
-  
-  getMaxSlotPlayer(){
+
+  getMaxSlotPlayer() {
     return new Promise((resolve, reject) => {
-      this.req.get('info.json',{
+      this.req.get('info.json', {
         headers: {
           'Accept': 'application/json',
           'Accept-Charset': 'utf-8'
         }
-	  }, (err, res, body) => {
+      }, (err, res, body) => {
         resolve(JSON.parse(body).vars.sv_maxClients)
-	  });
-	});
+      });
+    });
   };
-  
-  getVersion(){
+
+  getVersion() {
     return new Promise((resolve, reject) => {
-      this.req.get('info.json',{
+      this.req.get('info.json', {
         headers: {
           'Accept': 'application/json',
           'Accept-Charset': 'utf-8'
         }
-	  }, (err, res, body) => {
+      }, (err, res, body) => {
         resolve(JSON.parse(body).version)
-	  });
-	});
+      });
+    });
   };
-  
-  getTags(){
+
+  getTags() {
     return new Promise((resolve, reject) => {
-      this.req.get('info.json',{
+      this.req.get('info.json', {
         headers: {
           'Accept': 'application/json',
           'Accept-Charset': 'utf-8'
         }
-	  }, (err, res, body) => {
+      }, (err, res, body) => {
         resolve(JSON.parse(body).vars.tags)
-	  });
-	});
+      });
+    });
   };
-  
-  getresources(){
+
+  getresources() {
     return new Promise((resolve, reject) => {
-      this.req.get('info.json',{
+      this.req.get('info.json', {
         headers: {
           'Accept': 'application/json',
           'Accept-Charset': 'utf-8'
         }
-	  }, (err, res, body) => {
+      }, (err, res, body) => {
         resolve(JSON.parse(body).resources)
-	  });
-	});
+      });
+    });
   };
-  
-  getLocale(){
+
+  getLocale() {
     return new Promise((resolve, reject) => {
-      this.req.get('info.json',{
+      this.req.get('info.json', {
         headers: {
           'Accept': 'application/json',
           'Accept-Charset': 'utf-8'
         }
-	  }, (err, res, body) => {
+      }, (err, res, body) => {
         resolve(JSON.parse(body).vars.locale)
-	  });
-	});
+      });
+    });
   };
-  
-  getLicense(){
+
+  getLicense() {
     return new Promise((resolve, reject) => {
-      this.req.get('info.json',{
+      this.req.get('info.json', {
         headers: {
           'Accept': 'application/json',
           'Accept-Charset': 'utf-8'
         }
-	  }, (err, res, body) => {
+      }, (err, res, body) => {
         resolve(JSON.parse(body).vars.sv_licenseKeyToken)
-	  });
-	});
+      });
+    });
   };
 };
